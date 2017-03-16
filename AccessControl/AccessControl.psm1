0c7d9b4fef170853fc66a748bbb0b260d69b8f62
lmmarsano <luis.marsano@gmail.com>
2017-03-16 00:07:06 -0400

    Add Invoke-AsOwner

--- AccessControl/AccessControl.psm1
+++ AccessControl/AccessControl.psm1
@@ -1,6 +1,279 @@
-<#
-	My Function
-#>
-function Get-Function {
+Set-StrictMode -Version latest
 
+Enum ComAccessMask {
+	Execute = 1
+	ExecuteLocal = 2
+	ExecuteRemote = 4
+	ActivateLocal = 8
+	ActivateRemote = 0x10
+}
+
+<#
+[System.Security.AccessControl.AceFlags]
+Enum AceFlags {
+	ObjectInherit = 1
+	ContainerInherit = 2
+	NoPropagateInherit = 4
+	InheritOnly = 8
+	Inherited = 0x10
+}
+#>
+New-Variable -Name NtAdministrators `
+             -Description 'Builtin Administrators Group' `
+             -Option ReadOnly `
+             -Value ([System.Security.Principal.NTAccount]::new('BUILTIN', 'Administrators'))
+<#
+.Synopsis
+   Temporarily own registry keys and access their ACLs.
+.DESCRIPTION
+   The function retrieves a registry key's ACL and temporarily swaps its owner during a scriptblock call.
+
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Invoke-AsOwner {
+	[CmdletBinding()]
+	[Alias()]
+	[OutputType([System.Security.AccessControl.CommonObjectSecurity])]
+	Param(
+		# Key Registry key to provide ACL.
+		[Parameter( ParameterSetName='RegistryKey'
+		          , Mandatory=$true
+		          , ValueFromPipeline=$true
+		          )]
+		[Microsoft.Win32.RegistryKey]
+		$Key,
+
+		# Key Path of registry key to provide ACL.
+		[Parameter( ParameterSetName='RegistryPath'
+		          , Mandatory=$true
+		          , ValueFromPipeline=$true
+		          )]
+		[ValidateScript({Test-Path -LiteralPath $_})]
+		[string]
+		$KeyPath,
+
+		# ScriptBlock <[Microsoft.Win32.RegistryKey],[System.Security.AccessControl.CommonObjectSecurity]> scriptblock called to alter temporarily owned key and ACL.
+		[Parameter(Mandatory=$true)]
+		[System.Management.Automation.ScriptBlock]
+		$ScriptBlock,
+
+		# Owner Temporary key owner during invocation. Default: current user.
+		[Parameter()]
+		[System.Security.Principal.IdentityReference]
+		$Owner=[System.Security.Principal.WindowsIdentity]::GetCurrent().User
+	)
+
+	Begin {
+		$priv = Get-Privilege
+		$priv0 = Get-Privilege
+		$priv.Enable([Pscx.Interop.TokenPrivilege]::Restore)
+		Set-Privilege -Privileges $priv
+	} Process {
+		Write-Debug -Message ('Bound Parameters:{0}' -f (Out-String -InputObject $PSBoundParameters))
+		Write-Debug -Message ('Parameter Set: {0}' -f $PsCmdlet.ParameterSetName)
+		switch ($PsCmdlet.ParameterSetName) {
+			'RegistryKey' {}
+			'RegistryPath' { $Key = Get-Item -LiteralPath $KeyPath }
+			Default { throw [System.ArgumentException]::new('ParameterSetName {0} does not exist' -f $PsCmdlet.ParameterSetName) }
+		}
+		$acl = $Key.GetAccessControl()
+		$backup = $Key.GetAccessControl()
+		$original = $acl.GetOwner([System.Security.Principal.NTAccount])
+		$acl.SetOwner($Owner)
+		& $ScriptBlock -Key $Key -Acl $acl
+		try {
+			$Key.SetAccessControl($acl)
+		} catch [System.Management.Automation.MethodInvocationException] {
+			if ($_.Exception.InnerException -is [System.UnauthorizedAccessException]) {
+				$Key = Get-RegistryKey -Key $Key
+				$Key.SetAccessControl($acl)
+			} else {
+				throw
+			}
+		}
+		$acl.SetOwner($original)
+		$Key.SetAccessControl($acl)
+		$acl
+	} End {
+		Set-Privilege -Privileges $priv0
+	}
+}
+
+function Get-SecurityDescriptor {
+	[CmdletBinding(DefaultParameterSetName='ByPath', HelpUri='http://go.microsoft.com/fwlink/?LinkID=113305')]
+	param(
+		[Parameter(ParameterSetName='ByPath', Position=0, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+		[ValidateNotNullOrEmpty()]
+		[string[]]
+		${Path},
+
+		[Parameter(ParameterSetName='ByInputObject', Mandatory=$true)]
+		[psobject]
+		${InputObject},
+
+		[Parameter(ParameterSetName='ByLiteralPath', ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+		[Alias('PSPath')]
+		[ValidateNotNullOrEmpty()]
+		[string[]]
+		${LiteralPath},
+
+		[System.Security.AccessControl.AccessControlSections]
+		$AccessControlSections=[System.Security.AccessControl.AccessControlSections]::All -band -bnot [System.Security.AccessControl.AccessControlSections]::Audit,
+
+		[switch]
+		${Audit},
+
+		[switch]
+		${AllCentralAccessPolicies},
+
+		[string]
+		${Filter},
+
+		[string[]]
+		${Include},
+
+		[string[]]
+		${Exclude})
+
+	begin {
+	if ($Audit) {
+		$AccessControlSections = $AccessControlSections -bor [System.Security.AccessControl.AccessControlSections]::Audit
+	}
+	'AccessControlSections','AllCentralAccessPolicies' | % {
+		$PSBoundParameters.Remove($_)
+	}
+	} process {
+	@(switch ($x) {
+		'ByInputObject' { $InputObject }
+		{$_ -in 'ByPath','ByLiteralPath'} { Get-Item @PSBoundParameters }
+		Default {}
+	}) | % { $_.GetAccessControl($AccessControlSections) }
+	} end {
+		@(switch ($x) {
+			'ByInputObject' { $InputObject }
+			{$_ -in 'ByPath','ByLiteralPath'} { Get-Item @PSBoundParameters }
+			Default {}
+		}) | % { $_.GetAccessControl($AccessControlSections) }
+	}
+}
+<#
+
+.ForwardHelpTargetName Microsoft.PowerShell.Security\Get-Acl
+.ForwardHelpCategory Cmdlet
+
+#>
+
+function Set-SecurityDescriptor {
+	[CmdletBinding(DefaultParameterSetName='ByPath', SupportsShouldProcess=$true, ConfirmImpact='Medium', SupportsTransactions=$true, HelpUri='http://go.microsoft.com/fwlink/?LinkID=113389')]
+	param(
+		[Parameter(ParameterSetName='ByPath', Mandatory=$true, Position=0, ValueFromPipelineByPropertyName=$true)]
+		[string[]]
+		${Path},
+
+		[Parameter(ParameterSetName='ByInputObject', Mandatory=$true, Position=0, ValueFromPipelineByPropertyName=$true)]
+		[psobject]
+		${InputObject},
+
+		[Parameter(ParameterSetName='ByLiteralPath', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+		[Alias('PSPath')]
+		[string[]]
+		${LiteralPath},
+
+		[Parameter(ParameterSetName='ByInputObject', Mandatory=$true, Position=1, ValueFromPipeline=$true)]
+		[Parameter(ParameterSetName='ByLiteralPath', Mandatory=$true, Position=1, ValueFromPipeline=$true)]
+		[Parameter(ParameterSetName='ByPath', Mandatory=$true, Position=1, ValueFromPipeline=$true)]
+		[System.Object]
+		${AclObject},
+
+		[Parameter(ParameterSetName='ByPath', Position=2, ValueFromPipelineByPropertyName=$true)]
+		[Parameter(ParameterSetName='ByLiteralPath', Position=2, ValueFromPipelineByPropertyName=$true)]
+		[string]
+		${CentralAccessPolicy},
+
+		[Parameter(ParameterSetName='ByPath')]
+		[Parameter(ParameterSetName='ByLiteralPath')]
+		[switch]
+		${ClearCentralAccessPolicy},
+
+		[switch]
+		${Passthru},
+
+		[string]
+		${Filter},
+
+		[string[]]
+		${Include},
+
+		[string[]]
+		${Exclude})
+
+	begin
+	{
+		$outBuffer = $null
+		if ($PSBoundParameters.TryGetValue('OutBuffer', [ref]$outBuffer))
+		{
+			$PSBoundParameters['OutBuffer'] = 1
+		}
+		$wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand('Microsoft.PowerShell.Security\Set-Acl', [System.Management.Automation.CommandTypes]::Cmdlet)
+		$scriptCmd = {& $wrappedCmd @PSBoundParameters }
+		$steppablePipeline = $scriptCmd.GetSteppablePipeline($myInvocation.CommandOrigin)
+		$steppablePipeline.Begin($PSCmdlet)
+	}
+
+	process
+	{
+		$steppablePipeline.Process($_)
+	}
+
+	end
+	{
+		$steppablePipeline.End()
+	}
+}
+<#
+
+.ForwardHelpTargetName Microsoft.PowerShell.Security\Set-Acl
+.ForwardHelpCategory Cmdlet
+
+#>
+
+<#
+.Synopsis
+	Returns writable registry key.
+.DESCRIPTION
+	Long description
+.EXAMPLE
+	Example of how to use this cmdlet
+.EXAMPLE
+	Another example of how to use this cmdlet
+#>
+filter Get-RegistryKey {
+	[CmdletBinding()]
+	[Alias()]
+	[OutputType([Microsoft.Win32.RegistryKey])]
+	Param(
+		# Key Registry key.
+		[Alias('InputObject')]
+		[Parameter( ParameterSetName='RegistryKey'
+		          , Mandatory=$true
+		          , ValueFromPipeline=$true
+		          )]
+		[Microsoft.Win32.RegistryKey]
+		$Key,
+
+		# ReadOnly Restrict returned key's access to read-only.
+		[switch]
+		$ReadOnly
+	)
+	$root,$subkey = $Key.Name.Split('\',2)
+	$root = $root.Split('_',2)[1].Replace('_','')
+	Write-Verbose -Message ('Accessing root {0} subkey {1}' -f $root,$subkey)
+	$Key = [Microsoft.Win32.Registry]::$root
+	if ($subkey) {
+	$Key = $Key.OpenSubKey($subkey, !$ReadOnly)
+	}
+	$Key
 }
--- AccessControl/AccessControl.tests.ps1
+++ AccessControl/AccessControl.tests.ps1
@@ -3,11 +3,128 @@
 # You need a unit test framework such as Pester to run PowerShell Unit tests. 
 # You can download Pester from http://go.microsoft.com/fwlink/?LinkID=534084
 #
-
-Describe "Get-Function" {
+Set-StrictMode -Version latest
+#region Before All
+$ModuleName = [io.path]::GetFileName($PSScriptRoot)
+Import-Module "$PSScriptRoot\$ModuleName.psd1" -Scope Local -Force
+$TestKeyPath = 'registry::HKEY_CURRENT_USER\SOFTWARE\Test'
+$TrustedInstaller = [System.Security.Principal.NTAccount]::new('NT SERVICE','TrustedInstaller')
+#endregion
+#Before Each Context
+$bec = {
+	$TestKey = New-Item -Path $TestKeyPath
+	$acl = $TestKey.GetAccessControl() #[System.Security.AccessControl.AccessControlSections]::None
+	$acl.SetOwner($TrustedInstaller)
+	$priv = Get-Privilege
+	$priv0 = Get-Privilege
+	$priv.Enable([Pscx.Interop.TokenPrivilege]::Restore)
+	try {
+		Set-Privilege -Privileges $priv
+		$TestKey.SetAccessControl($acl)
+	} finally {
+		Set-Privilege -Privileges $priv0
+	}
+}
+#After Each Context
+$aec = {
+	Remove-Item -LiteralPath $TestKeyPath
+}
+Describe "Invoke-AsOwner" {
 	Context "Function Exists" {
-		It "Should Return" {
-		
+		BeforeAll {
+			. $bec
+			$params = [string[]]::new(2)
+			$retval = Invoke-AsOwner -Key $TestKey `
+						-ScriptBlock {
+				Param(
+					[Microsoft.Win32.RegistryKey]$Key
+				, [System.Security.AccessControl.RegistrySecurity]$Acl
+				)
+				$params[0] = $Key.ToString()
+				Write-Debug -Message ('ScriptBlock Parameters:{0}' -f (Out-String -InputObject $PSBoundParameters))
+			}
+			Invoke-AsOwner -KeyPath $TestKeyPath `
+			               -ScriptBlock {
+				Param(
+				  [Microsoft.Win32.RegistryKey]$Key
+				, [System.Security.AccessControl.RegistrySecurity]$Acl
+				)
+				$params[1] = $Key.ToString()
+				Write-Debug -Message ('ScriptBlock Parameters:{0}' -f (Out-String -InputObject $PSBoundParameters))
+			}
+			$function = Get-Item -LiteralPath function:\Invoke-AsOwner
+		}
+		AfterAll {
+			. $aec
+		}
+		It "Returns values of type System.Security.AccessControl" {
+			Should -ActualValue $retval `
+							-BeOfType `
+							-ExpectedType System.Security.AccessControl.CommonObjectSecurity
+		}
+		It 'Returns Key''s ACL' {
+			Should -ActualValue $TestKey.GetAccessControl().GetSecurityDescriptorSddlForm([System.Security.AccessControl.AccessControlSections]::All) `
+							-Be `
+							-ExpectedValue $retval.GetSecurityDescriptorSddlForm([System.Security.AccessControl.AccessControlSections]::All)
+		}
+		It 'Restores Owner' {
+			Should -ActualValue $TestKey.GetAccessControl().GetOwner([System.Security.Principal.NTAccount]) `
+					-Be `
+					-ExpectedValue $TrustedInstaller
+		}
+		'Key','KeyPath','ScriptBlock' | % {
+			$key = $_
+			It ('Accepts Parameter {0}' -f $key) {
+			Should -ActualValue $function.Parameters.ContainsKey($key) `
+					-Be `
+					-ExpectedValue $true
+			}
+		}
+		It 'Treats key and its path alike' {
+			Should -ActualValue $params[0] `
+					-Be `
+					-ExpectedValue $params[1]
+		}
+	}
+	Context 'ScriptBlock' {
+		BeforeAll {
+			. $bec
+			$oldAccess = Out-String -InputObject $acl.GetAccessRules($true, $true, [System.Security.Principal.NTAccount])
+			Invoke-AsOwner -Key $TestKey `
+						-ScriptBlock {
+				Param(
+				  [Microsoft.Win32.RegistryKey]$Key
+				, [System.Security.AccessControl.RegistrySecurity]$Acl
+				)
+				Write-Debug -Message ('ScriptBlock Parameters:{0}' -f (Out-String -InputObject $PSBoundParameters))
+				Set-Variable -Scope 1 -Name sbparam -Value $PSBoundParameters
+				Set-Variable -Scope 1 -Name tempOwner -Value $Acl.GetOwner([System.Security.Principal.NTAccount])
+				$Acl.AddAccessRule(
+					[System.Security.AccessControl.RegistryAccessRule]::new(
+					  [System.Security.Principal.NTAccount]::new('BUILTIN','Administrators') #$TrustedInstaller
+					, [System.Security.AccessControl.RegistryRights]::FullControl
+					, [System.Security.AccessControl.AccessControlType]::Allow
+					)
+				)
+				Write-Debug -Message ('New access rules:{0}' -f (Out-String -InputObject $Acl.GetAccessRules($true, $false, [System.Security.Principal.NTAccount])))
+			}
+		}
+		AfterAll { . $aec }
+		'Key','Acl' | % {
+			$key = $_
+			It ('Receives {0} parameter' -f $key) {
+				Should -ActualValue $sbparam.ContainsKey($key) -Be -ExpectedValue $true
+			}
+		}
+		It 'Receives ACL with temporary owner' {
+			Should -ActualValue $tempOwner `
+			       -Be `
+			       -ExpectedValue ([System.Security.Principal.WindowsIdentity]::GetCurrent().User.Translate([System.Security.Principal.NTAccount]))
+		}
+		It 'Can change object security' {
+			Should -ActualValue (Out-String -InputObject $TestKey.GetAccessControl().GetAccessRules($true, $false, [System.Security.Principal.NTAccount])) `
+			       -Not -Be `
+			       -ExpectedValue $oldAccess
 		}
 	}
 }
